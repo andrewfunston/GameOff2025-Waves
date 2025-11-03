194a0ca183a4a6176f78be0c15b86ed5099c5327
Andy <andrew.funston@gmail.com>
2025-11-03 12:31:32 -0600

    enemy can move

---
+++ game-off-test-1/enemy.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var mDirection = 1;
+
+@onready var right_ray_cast_2d = $RightRayCast2D
+@onready var left_ray_cast_2d_2 = $LeftRayCast2D2
+
+
+func _process(delta):
+	if right_ray_cast_2d.is_colliding() or left_ray_cast_2d_2.is_colliding() :
+		mDirection*=-1
+			
+	position.x += mDirection * delta * 100.0
