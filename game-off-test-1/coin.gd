9e7420de3c679466ef6cf524c0fbf0860a8384f9
Andy <andrew.funston@gmail.com>
2025-11-05 07:51:55 -0600

    finish fixing stuff

--- game-off-test-1/coin.gd
+++ game-off-test-1/coin.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var mCoinNoise = $AudioStreamPlayer2D
+@onready var mAnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body):
 	game_manager.addCoin()
-	queue_free()
+	mAnimationPlayer.play("pickup")
