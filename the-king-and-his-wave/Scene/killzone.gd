81c4dbf22ab897efe840a846ac8265c523df6cd6
justinsewak <justin.sewak@gmail.com>
2025-11-13 10:16:40 -0500

    tried making an enemy
    won't die

---
+++ the-king-and-his-wave/Scene/killzone.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+func _on_body_entered(body) -> void:
+	print("dead")
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
