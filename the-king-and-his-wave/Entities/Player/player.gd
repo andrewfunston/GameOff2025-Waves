1f521556c0d81e5e01e6720917f4a585888c2f65
justinsewak <justin.sewak@gmail.com>
2025-11-19 13:39:01 -0500

    double jump and ocean foregrond
    we can double jump and i added the ocean foreground that must have gotten lost. I did it correctly by making a new node

--- the-king-and-his-wave/Entities/Player/player.gd
+++ the-king-and-his-wave/Entities/Player/player.gd
@@ -5,14 +5,19 @@ enum States {idle, walking, running, jumping, falling, landing}
 const SPEED = 200.0
 const ACCEL = 400.0
 const JUMP_VELOCITY = -300.0
+const MAX_JUMPS:int = 2 # NEW: 1 normal jump + 1 double jump
 
 @onready var mAnimatedSprite2D = $AnimatedSprite2D
 
 var mState:States = States.idle
 var mLastVelocity:Vector2
 var mIdleTime:float = 0
+var mJumpsLeft:int = MAX_JUMPS # NEW: how many jumps we have left before landing
 
-func _ready():# Called when the node enters the scene tree for the first time.
+func _ready(): # Called when the node enters the scene tree for the first time.
+	# NEW: start with full jumps
+	mJumpsLeft = MAX_JUMPS
+	
 	# when the start the level if we're not on the floor we need to start falling.
 	if not is_on_floor():
 		startFall()
@@ -47,38 +52,54 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# NEW: removed the old ground-only jump here.
+	# Jump input is now handled in the state functions so we can also jump in mid-air.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# 	velocity.y = JUMP_VELOCITY
 
 	mLastVelocity = velocity
 	move_and_slide()
 
 func processFalling():
 	if is_on_floor():
+		# NEW: we landed, so reset available jumps
+		mJumpsLeft = MAX_JUMPS
+		
 		# we have an animation for "landing" we may want to add in.
 		if velocity.x == 0:
 			startIdle()
 		else:
 			startWalk()
-	
+	else:
+		# NEW: allow jump while falling (double jump)
+		if Input.is_action_just_pressed("ui_accept") and mJumpsLeft > 0:
+			startJump()
+
 func processIdle(delta):
 	mIdleTime+=delta
 	if(mIdleTime>5 && mAnimatedSprite2D.animation == "idle"):
 		mAnimatedSprite2D.play("longIdle")
 		
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# NEW: jump if we have any jumps left
+	if Input.is_action_just_pressed("ui_accept") and mJumpsLeft > 0:
 		startJump()
 	elif velocity.x != 0:
 		startWalk()
 
 func processJump():
+	# NEW: allow another jump while already jumping (double jump)
+	if Input.is_action_just_pressed("ui_accept") and mJumpsLeft > 0:
+		startJump()
+		return
+	
 	# velocity.y < 0 means that we're still "jumping"
-	# velocity.x > 0 means we've started falling
+	# velocity.y >= 0 means we've started falling
 	if velocity.y >= 0:
 		startFall()
 
 func processWalking():
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# NEW: use jump counter instead of "only if on floor"
+	if Input.is_action_just_pressed("ui_accept") and mJumpsLeft > 0:
 		startJump()
 	elif !is_on_floor():
 		startFall()
@@ -89,7 +110,8 @@ func processWalking():
 		startIdle()
 
 func processRunning():
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# NEW: use jump counter instead of "only if on floor"
+	if Input.is_action_just_pressed("ui_accept") and mJumpsLeft > 0:
 		startJump()
 	elif !is_on_floor():
 		startFall()
@@ -105,6 +127,11 @@ func startRun():
 	mAnimatedSprite2D.play("running")	
 	
 func startJump():
+	# NEW: only jump if we still have jumps left
+	if mJumpsLeft <= 0:
+		return
+	
+	mJumpsLeft -= 1 # we just used one of our jumps (first or double)
 	velocity.y = JUMP_VELOCITY
 	mState = States.jumping
 	mAnimatedSprite2D.play("jump")
